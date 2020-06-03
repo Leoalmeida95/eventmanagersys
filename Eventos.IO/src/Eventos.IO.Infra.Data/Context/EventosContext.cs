18e9dad8d08cf811e4033877c6f7714377514aa0
leoalmeida95 <leo.almeida.silva@hotmail.com>
2020-06-03 00:59:57 -0300

    criando o EventoContext no projeto de Infra

---
+++ Eventos.IO/src/Eventos.IO.Infra.Data/Context/EventosContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Eventos.IO.Infra.Data.Context
+{
+    public class EventosContext : DbContext
+    {
+        public DbSet<Evento.IO.Domain.Eventos.Evento> Eventos { get; set; }
+        public DbSet<Evento.IO.Domain.Organizadores.Organizador> Organizadores { get; set; }
+        public DbSet<Evento.IO.Domain.Eventos.Categoria> Categorias { get; set; }
+        public DbSet<Evento.IO.Domain.Eventos.Endereco> Enderecos { get; set; }
+    }
+}
